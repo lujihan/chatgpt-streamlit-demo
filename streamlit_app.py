1fed77ce7000298df7a730e72c56178be6926cd3
lujihan <76890755+lujihan@users.noreply.github.com>
2023-02-11 13:56:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def ask():
 st.header("ChatGPT老胡模拟器")
 prompt = ask()
 if prompt:
-	chatbot.ask("你要像胡锡进那样回答我的问题。开头加上“老胡认为”，中间加上“但是，老胡还认为”，并且后半段的回答要用来反驳前半段的回答。")
+	chatbot.ask("")
 	output = chatbot.ask(prompt)["choices"][0]["text"]
 	st.write(output)
 	chatbot.reset()
